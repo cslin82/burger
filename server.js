81fb97c38599e8e3581d60aac1c6d29a5ad539a3
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-05-16 20:21:47 -0400

    Create express/handlebars framework

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 
 var app = express();
 
+var PORT = process.env.PORT || 8080;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+require("./routes/index")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
