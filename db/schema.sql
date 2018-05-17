6a8246e39763daf5cd979178988d3aacc603883b
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-05-17 16:07:16 -0400

    Write sql schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -10,3 +10,10 @@ USE burgers_db;
 -- burger_name: a string.
 -- devoured: a boolean.
 
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Two all-beef patties, special sauce, lettuce, cheese, pickles, onions on a sesame seed bun');
+INSERT INTO burgers (burger_name) VALUES ('2x2 Animal style');
+INSERT INTO burgers (burger_name) VALUES ('Whopper');
+INSERT INTO burgers (burger_name) VALUES ('Veggie with bacon');
+INSERT INTO burgers (burger_name) VALUES ('Veggie');
